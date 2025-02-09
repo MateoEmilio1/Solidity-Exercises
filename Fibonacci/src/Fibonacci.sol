03d57f12e73a52aa48dc769f2df8c0491362e4fc
MateoEmilio1 <mateoyastor60@gmail.com>
2025-02-08 21:10:11 -0300

    testing contracts

--- Fibonacci/src/Fibonacci.sol
+++ Fibonacci/src/Fibonacci.sol
@@ -6,12 +6,25 @@ contract Fibonacci {
         This exercise assumes you understand what Fibonacci sequence is.
         1. Function `fibonacci` takes a uint256 as argument and returns nth fibonacci number.
         
-        Fibonacci sequence are 0,1,1,2,3,5,8,13,....
+        Fibonacci sequence is: 0,1,1,2,3,5,8,13,....
         
-        calling fibonacci(6) would return 8, because the 6th Fibonacci number is 8.
+        Calling fibonacci(6) should return 8, because the 6th Fibonacci number is 8.
     */
 
-    function fibonacci(uint256 _position) public view returns (uint256) {
-        // your code here
+    function fibonacci(uint256 _position) public pure returns (uint256) {
+        if (_position == 0) return 0;
+        if (_position == 1) return 1;
+
+        uint256 a = 0;
+        uint256 b = 1;
+        uint256 result;
+
+        for (uint256 i = 2; i <= _position; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+
+        return result;
     }
 }
